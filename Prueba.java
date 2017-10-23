b9922d8c4bbfc40f390f9f305f995f4726186189
adrianjguerrero <glact.adrian11@gmail.com>
2017-10-22 20:26:11 -0430

    Agregue un mensaje que solo se ve en rama adicional

--- Prueba.java
+++ Prueba.java
@@ -6,7 +6,9 @@ public class Prueba {
 
     
     public static void main(String[] args) {
-     JOptionPane.showMessageDialog(null,"Prueba","Pruebaagria",2);
+     JOptionPane.showMessageDialog(null,"Prueba","Pruebaagriaa",2);
+     JOptionPane.showMessageDialog(null,"Nuevo mensaje","ohsi",1);
+     JOptionPane.showMessageDialog(null,"Mensaje solo visualisabre en rama 'adiciona'","Adicional",1);
     }
     
 }
