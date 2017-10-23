058fb0f3b433be6b9e6e30a11a15e2b873f0fee0
adrianjguerrero <glact.adrian11@gmail.com>
2017-10-22 19:50:30 -0430

    Iniciamos

---
+++ Prueba.java
@@ -0,0 +1,12 @@
+
+package prueba;
+import javax.swing.JOptionPane;
+
+public class Prueba {
+
+    
+    public static void main(String[] args) {
+     JOptionPane.showMessageDialog(null,"Prueba","Pruebaagria",1);
+    }
+    
+}
